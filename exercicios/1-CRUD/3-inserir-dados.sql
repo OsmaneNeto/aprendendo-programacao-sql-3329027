a68ebb654b3f9e016fd17fa894b6f12c09211561
Osmane Barbosa Neto <135390417+OsmaneNeto@users.noreply.github.com>
2025-03-20 19:03:41 +0000

    alteracao

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,14 @@
 -- Insira 3 classes salariais na tabela salario_classe
+INSERT INTO salario_classe (id, salario, nivel)
+VALUES
+(4, 3500, 'estagiário'),
+(5, 4700, 'iniciante'),
+(6, 25000, 'Eu em algum dia');
 
+DELETE salario_classe where 
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+
+PRAGMA table_info(salario_classe);
+
+SELECT * FROM salario_classe;
